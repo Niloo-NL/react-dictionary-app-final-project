ae02ccedb0c41155169550e87ae15136b05f5ab6
Niloofar <niloofar.navaieelavasani@gmail.com>
2022-12-26 12:40:33 +0100

    added dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
+
+    //documentation : https://dictionaryapi.dev/
+    let apiUrl = ` https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord} `;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleChange(event) {

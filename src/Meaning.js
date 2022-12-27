dcce7a3c52385353514c170a0bdce5471f27350e
Niloofar <niloofar.navaieelavasani@gmail.com>
2022-12-27 11:54:56 +0100

    Added Synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   return (
     <div className="meaning">
@@ -8,10 +8,11 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              {definition.definition}
+              <strong>Definition</strong> : {definition.definition}
               <br></br>
-              <em> {definition.example}</em>
+              <strong>Example</strong> : <em> {definition.example}</em>
             </p>{" "}
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}

d40b544d96ae4e5d0a163e1578693b90786a536e
Niloofar <niloofar.navaieelavasani@gmail.com>
2022-12-28 21:53:59 +0100

    Changed Phonetics audio icon

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,10 +1,22 @@
 import React from "react";
-import ReactAudioPlayer from "react-audio-player";
+//import ReactAudioPlayer from "react-audio-player";
+import AudioPlayer from "react-h5-audio-player";
 
 export default function Phonetic(props) {
   return (
     <div className="phonetic">
-      <ReactAudioPlayer src={props.input.audio} autoPlay={false} controls />
+      <AudioPlayer
+        autoPlay={false}
+        src={props.input.audio}
+        onPlay={(e) => console.log("onPlay")}
+        showSkipControls={false}
+        hasDefaultKeyBindings={false}
+        showJumpControls={false}
+        customVolumeControls={[]}
+        customAdditionalControls={[]}
+        defaultCurrentTime=""
+        defaultDuration=""
+      />
       <br></br>
       {props.input.text}
     </div>

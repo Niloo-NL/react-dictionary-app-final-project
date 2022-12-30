ba9ce6a1f2724c65be992ef8d5b7252593cd5b73
Niloofar <niloofar.navaieelavasani@gmail.com>
2022-12-30 22:21:58 +0100

    Added background image and colors

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="App">
+      <header className="App-header"></header>
+      <h1>Dictionary</h1>
       <div className="container">
-        <header className="App-header"></header>
-        <h1>Dictionary App</h1>
         <main>
           <Dictionary />
         </main>
